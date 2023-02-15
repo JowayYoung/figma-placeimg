600fa1505d5d0fc3f71729b885fa3bbf58e8b73a
yangzewei <yangzewei@corp.netease.com>
2023-02-15 10:48:16 +0800

    调整输出位置

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,16 +1,12 @@
 import { resolve } from "path";
-// import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
-
-// const __filename = fileURLToPath(import.meta.url); // eslint-disable-line
-// const __dirname = dirname(__filename); // eslint-disable-line
 
 export default defineConfig({
 	build: {
 		lib: {
 			entry: resolve(__dirname, "src/index.ts"),
-			fileName: format => `core.${format}.js`,
-			name: "core"
+			fileName: format => `index.${format}.js`,
+			name: "FigmaPlaceimg"
 		}
 	},
 	plugins: []
--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -4,6 +4,9 @@ import ReactPlugin from "@vitejs/plugin-react";
 import { viteSingleFile as ViteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
+	build: {
+		outDir: join(__dirname, "dist")
+	},
 	plugins: [
 		ReactPlugin(),
 		ViteSingleFile()
